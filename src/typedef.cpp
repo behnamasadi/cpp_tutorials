51cf9e151d3d31659e33d6d90e4b83f654bddd98
Behnam Asadi <behnam.asadi@gmail.com>
2020-02-03 09:55:45 +0100

    Update typedef.cpp

--- src/typedef.cpp
+++ src/typedef.cpp
@@ -5,6 +5,24 @@
 
 
 /*
+
+There are two ways of declaring new type aliases in modern C++. The first and traditional one is with the typedef keyword:
+
+1)typedef [original-type] [your-alias];
+typedef int Pixel;
+typedef std::map<std::string, std::vector<std::string>> Map;
+
+2)The other one, introduced in C++11, is with the using keyword (using works best with templates):
+using [your-alias] = [original-type];
+
+using Pixel = int;
+using Map   = std::map<std::string, std::vector<std::string>>;
+
+template <typename T> using Matrix= std::vector<std::vector<T>> ;
+
+
+
+
 typedef void (*Something)()
 
 To make it a function that takes no arguments (in C), you'd use:
