c31ca5c5b25d44341e64ec6006580b29efffa390
Behnam Asadi <behnam.asadi@gmail.com>
2020-02-14 13:42:35 +0100

    Create basic_input_output_cin_scanf_gets_getline_printf.cpp

---
+++ src/basic_input_output_cin_scanf_gets_getline_printf.cpp
@@ -0,0 +1,7 @@
+https://www.geeksforgeeks.org/cin-get-in-c-with-examples/
+https://www.geeksforgeeks.org/basic-input-output-c/
+https://www.geeksforgeeks.org/endl-vs-n-in-cpp/
+https://www.geeksforgeeks.org/problem-with-scanf-when-there-is-fgetsgetsscanf-after-it/
+https://www.geeksforgeeks.org/cincout-vs-scanfprintf/
+https://www.geeksforgeeks.org/how-to-use-getline-in-c-when-there-are-black-lines-in-input/
+https://www.geeksforgeeks.org/difference-between-scanf-and-gets-in-c/
