7bd8cb1875cdefe613dc632db9418de78fdb3508
behnam.asadi <behnam.asadi@gmail.com>
2020-07-07 10:25:04 +0200

    multi thread updated

--- src/sandbox/src/main.cpp
+++ src/sandbox/src/main.cpp
@@ -1,23 +1,68 @@
 #include <iostream>
 #include <thread>
-//#include <chrono>         // std::chrono::seconds
-
 #include <vector>
 #include <algorithm>
+#include <functional>
+#include <condition_variable>
+#include <future>
+std::condition_variable cond ;
 
-void doWork()
+class Foo {
+public:
+    Foo() {
+
+    }
+
+    void first(std::function<void()> printFirst) {
+
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+    }
+
+    void second(std::function<void()> printSecond) {
+
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+    }
+
+    void third(std::function<void()> printThird) {
+
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+};
+
+void printFirst()
 {
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    std::cout<<"do work" <<std::endl;
-    std::this_thread::get_id();
+    std::cout<<"first";
 }
 
+void printSecond()
+{
+    std::cout<<"second";
+}
+
+void printThird()
+{
+    std::cout<<"third";
+}
 
 
 int main()
 {
-    std::thread worker(doWork);
-    std::cout<<"main thread" <<std::endl;
-    //worker.join();
-    //std::cin.get();
+    /**/
+    Foo foo;
+    std::future<void>  threadA=std::async(&Foo::first,foo,printFirst);
+    threadA.get();
+    std::thread  threadB(&Foo::second,foo,printSecond);
+    std::thread  threadC(&Foo::third,foo,printThird);
+
+
+//    threadA.join();
+//    threadB.join();
+//    threadC.join();
+
 }
+
+
+
