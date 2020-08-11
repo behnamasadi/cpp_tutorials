c3b5d30e7a15a01113ff073d67cdbf9ec90565bd
behnam.asadi <behnam.asadi@gmail.com>
2020-08-11 16:32:40 +0200

    UPPERCASE to lowercase in cmake

---
+++ src/multithreading/thread_memory_error_detector.cpp
@@ -0,0 +1,7 @@
+/*
+https://valgrind.org/docs/manual/mc-manual.html
+https://www.valgrind.org/docs/manual/hg-manual.html/
+https://valgrind.org/docs/manual/drd-manual.html
+https://gperftools.github.io/gperftools/heapprofile.html
+*/
+int main(){}
