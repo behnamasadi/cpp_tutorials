d4d3cbbc034d15169d00e90b325419242d227654
Behnam <behnam.asadi@gmail.com>
2021-05-13 11:21:16 +0200

    cmake_tutorial moved into a new repository

--- src/sandbox/src/main.cpp
+++ src/sandbox/src/main.cpp
@@ -2,11 +2,35 @@
 #include <thread>
 #include <vector>
 #include <algorithm>
-#include <functional>
 #include <condition_variable>
 #include <future>
+#include <deque>
 
 /*
+https://www.educba.com/multithreading-interview-questions-c-plus-plus/
+https://www.geeksforgeeks.org/tag/cpp-multithreading/
+https://www.wisdomjobs.com/e-university/c-multithreading-developer-interview-questions.html
+multi thread interview questions c++
+https://career.guru99.com/top-40-multithreading-interview-questions-and-answers/
+https://www.indeed.com/career-advice/interviewing/multithreading-interview-questions
+https://quizlet.com/24524062/c-multithreading-practice-interview-questions-flash-cards/
+https://www.acodersjourney.com/top-20-cplusplus-multithreading-mistakes/
+https://www.modernescpp.com/index.php/c-core-guidelines-taking-care-of-your-child-thread
+c++ multi threading interview questions
+https://www.geeksforgeeks.org/c-plus-plus/?ref=shm
+c++ multithreading exercises
+https://www.bogotobogo.com/cplusplus/quiz_multithreading.php
+https://www.linuxtopia.org/online_books/programming_books/c++_practical_programming/c++_practical_programming_289.html
+https://leetcode.com/problemset/concurrency/
+c++ concurrency exercises
+https://softwareengineering.stackexchange.com/questions/32763/how-would-you-practice-concurrency-and-multi-threading
+https://www.reddit.com/r/cpp/comments/gcrp6f/concurrency_coding_exercise_ideas/
+https://cs61.seas.harvard.edu/site/2018/SynchEx/
+detect deadlock automatically
+
+https://thispointer.com/different-ways-to-iterate-over-a-set-in-c/
+
+
 //std::defer_lock, std::try_lock, std::adopt_lock
 Timed mutex
 try_lock_for and try_lock_until
@@ -23,53 +47,37 @@ unlock_shared()
 cond.wait_for
 Condition_variable_any
 
+
+https://livebook.manning.com/book/c-plus-plus-concurrency-in-action/chapter-2/4
 */
 
+
+#include <time.h>
 std::mutex mu;
-void worker1()
+int worker(int &input)
 {
-    for(int i=0;i<1000;i++)
-    {
-        std::lock_guard<std::mutex> locker(mu);
-
-        std::cout<<"worker1 is working, i="<<i <<std::endl;
-    }
+    std::this_thread::sleep_for(std::chrono::seconds(4));
+    return ++input;
 }
 
-void worker2()
+
+int main ()
 {
+    auto now=std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::cout<<std::string(ctime(&now))<<std::endl;
 
 
-    for(int i=1000;i>0;i--)
-    {
-        std::lock_guard<std::mutex> locker(mu);
-        std::cout<<"worker2 is working, i="<<i <<std::endl;
-    }
-}
+    time_t _tm =time(NULL );
 
-class myclass
-{
-    public:
-    void func(){    std::cout<<"func is working" <<std::endl;
-}
-    void  operator  ()()
-    {
-        std::cout<<"operator ()" <<std::endl;
-    }
-
-};
-
-int myfunc(int &x)
-{
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    return x++;
-}
-//https://www.geeksforgeeks.org/features-of-c17-with-examples/?ref=leftbar-rightbar
-int main()
-{
+    struct tm * curtime = localtime ( &_tm );
+    std::cout<<asctime(curtime);
 
 }
 
+
+//std::promise
+//std::future()
+//std::async
 
 
 
