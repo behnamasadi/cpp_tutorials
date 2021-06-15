7e6a6207bdd2d5bb975beac59a06aac6e38d1c2c
Behnam <behnam.asadi@gmail.com>
2021-06-15 17:27:44 +0200

    type dispatch_integral constant true false type added

---
+++ src/type_dispatch_integral_constant_true_false_type.cpp
@@ -0,0 +1,12 @@
+/*
+https://www.reddit.com/r/cpp_questions/comments/crbf62/is_stdtrue_type_stdfalse_type_the_c11_way_of/
+https://stackoverflow.com/questions/49205196/whats-the-reason-behind-having-stdintegral-constant
+https://stackoverflow.com/questions/20368187/when-would-i-use-stdintegral-constant-over-constexpr/36146050
+https://stackoverflow.com/questions/58694521/what-is-stdfalse-type-or-stdtrue-type
+
+
+*/
+int main()
+{
+
+}
