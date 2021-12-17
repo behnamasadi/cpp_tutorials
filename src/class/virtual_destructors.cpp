5061d4b3da75023f4eb0451c1881061bc85753a0
Behnam <behnam.asadi@gmail.com>
2021-12-18 00:19:31 +0100

    c++ 20 and virtual destructor added

---
+++ src/class/virtual_destructors.cpp
@@ -0,0 +1,6 @@
+//ref :https://stackoverflow.com/questions/461203/when-to-use-virtual-destructors
+
+int main()
+{
+
+}
---
+++ src/cpp_20.cpp
@@ -0,0 +1,4 @@
+//ref: https://github.com/AnthonyCalandra/modern-cpp-features
+int main()
+{
+}
