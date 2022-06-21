641eb68708e26b1c1ebfa913c53fa15f9f52eaa8
Behnam Asadi <behnam.asadi@gmail.com>
2022-06-21 14:32:14 +0200

    assert fixed

--- src/align.cpp
+++ src/align.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <iomanip>
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h> // for offsetof()
+
+//#pragma pack(1)
 
 
-class foo1
+struct Foo1
 {
     char c1;
     int i1;
 };
 
-class foo2
+struct Foo2
 {
     char c1;
     int i1;
@@ -15,7 +21,7 @@ class foo2
     int i2;
 };
 
-class foo3
+struct Foo3UnPacked
 {
     char c1;
     char c2;
@@ -23,7 +29,19 @@ class foo3
     int i2;
 };
 
-class alignas(8) foo4
+const int aligned_value=8;
+
+struct Foo3
+{
+    char c1 __attribute__((packed,aligned(aligned_value)));;
+    char c2 __attribute__((packed,aligned(aligned_value)));;
+    int i1 __attribute__((packed,aligned(aligned_value)));;
+    int i2 __attribute__((packed,aligned(aligned_value)));;
+};
+
+
+
+struct alignas(8) Foo4
 {
     char c1;
     int i1;
@@ -111,21 +129,70 @@ the reason is cpu fetch data from memory( our array) on the chunk size of cache_
 
 int main(int argc, char *argv[])
 {
-    std::cout<<"size of char: " <<sizeof(char)  <<std::endl;
-    std::cout<<"align of char: " <<alignof(char)  <<std::endl;
+    // std::cout<<"size of char: " <<sizeof(char)  <<std::endl;
+    // std::cout<<"align of char: " <<alignof(char)  <<std::endl;
+
+    // std::cout<<"size of int: " << sizeof(int)  <<std::endl;
+    // std::cout<<"align of int: " << alignof(int)  <<std::endl;
+
+    // std::cout<<"size of Foo1: " << sizeof(Foo1)  <<std::endl;
+    // std::cout<<"align of Foo1: " << alignof(Foo1)  <<std::endl;
+
+    // std::cout<< "size of Foo2: " <<sizeof(Foo2)  <<std::endl;
+    // std::cout<< "align of Foo2: " <<alignof(Foo2)  <<std::endl;
+
+
+
+
+    //std::cout<<sysconf (_SC_LEVEL1_DCACHE_LINESIZE)<<std::endl;
+
+
+    //Foo3 foo_object ={2,1,100,200} ; 
+
+    //Foo3 foo_object __attribute__((packed,aligned(1))) ;// ={2,1,100,200} ; 
+    Foo3 foo_object  ={2,1,100,200} ; 
+    //char c __attribute__((packed,aligned(4)));
+
+
+    std::cout<<"size of byte: " << sizeof(std::byte)  <<std::endl;
+
+    std::cout<<"size of char: " << sizeof(char)  <<std::endl;
+    std::cout<< "align of char: " <<alignof(char)  <<std::endl;
 
     std::cout<<"size of int: " << sizeof(int)  <<std::endl;
-    std::cout<<"align of int: " << alignof(int)  <<std::endl;
+    std::cout<< "align of int: " <<alignof(int)  <<std::endl;
 
-    std::cout<<"size of foo1: " << sizeof(foo1)  <<std::endl;
-    std::cout<<"align of foo1: " << alignof(foo1)  <<std::endl;
+    std::cout<<"size of Foo3: " << sizeof(Foo3)  <<std::endl;
+    std::cout<<"align of Foo3: " << alignof(Foo3)  <<std::endl;
 
-    std::cout<< "size of foo2: " <<sizeof(foo2)  <<std::endl;
-    std::cout<< "align of foo2: " <<alignof(foo2)  <<std::endl;
-    
-    std::cout<<"size of foo3: " << sizeof(foo3)  <<std::endl;
-    std::cout<<"align of foo3: " << alignof(foo3)  <<std::endl;
+        std::cout<<"size of Foo3: " << sizeof(std::word)  <<std::endl;
+
+    printf("offsetof(struct Foo3, c1) = %zu\n", offsetof(struct Foo3, c1));
+    printf("offsetof(struct Foo3, c2) = %zu\n", offsetof(struct Foo3, c2));
+    printf("offsetof(struct Foo3, i1) = %zu\n", offsetof(struct Foo3, i1));
+    printf("offsetof(struct Foo3, i2) = %zu\n", offsetof(struct Foo3, i2));
+
 
     //perfomanceBenchmarking();
+
+
+
+   
+
+
+    unsigned char memory_data;
+    for(std::size_t i=0;i<sizeof(Foo3);i++)
+    {
+        memory_data=*(((unsigned char*)&foo_object)+i);
+        //std::cout<< memory_data  <<std::endl;
+        //printf("%04x ",memory_data);
+        printf("%03hhu ",memory_data);
+
+
+        //std::cout<<  std::setbase(10) <<memory_data  <<std::endl;
+
+    }
+    printf("\n");
+
     return 0;
 }
