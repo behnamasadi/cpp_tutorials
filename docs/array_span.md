2af0a3c95c47c5c2a068aa78cbbb98bbadf461a2
Behnam Asadi <behnam.asadi@gmail.com>
2022-10-04 13:44:57 +0200

    array updated

--- docs/array_span.md
+++ docs/array_span.md
@@ -2,10 +2,203 @@
 
 
 
+## Size of Array
 
-[code](src/arrays.cpp)
+```cpp
+int array[] = { 1, 2, 3, 4, 5 };
+std::cout << "sizeof(array)="<<sizeof(array)<<std::endl; // prints 20 (5 elements * 4 bytes each)
+const int number_of_elemenets = sizeof(array)/sizeof(int);
+std::cout << "number of elemenets = "<<number_of_elemenets<<std::endl;
+```
+## Accessing Array Elements
+
+These are equal:  
+`array<=>&array[0]`  
+`array+index<=>&array[index]`  
+
+
+getting elements:
+```cpp
+std::cout<<"*array="<< *array<<std::endl;
+std::cout<<"*(array+2)="<< *(array+2)<<std::endl;
+
+
+for (int* p = array; p != array+number_of_elemenets; p++)
+{
+	std::cout << *p << std::endl;
+}
+```
+
+
+## Multidimensional Arrays
+
+`C` does not have true multidimensional arrays. you can have arrays of arrays, so like
+```cpp
+int first2DArray[3][5] ={
+{ 1, 2, 3, 4, 5, }, // row 0
+{ 6, 7, 8, 9, 10, }, // row 1
+{ 11, 12, 13, 14, 15 } // row 2
+};
+```
+Two-dimensional arrays with initializer lists can omit (only) the first size dimension(the most left one):
+```cpp
+int second2DArray[][5] ={{ 1, 2, 3, 4, 5, },{ 6, 7, 8, 9, 10, },{ 11, 12, 13, 14, 15 }};
+```
+or 
+
+```cpp
+    int **array;
+    array=new int *[3];
+    array[0]=new int[2];
+    array[1]=new int[4];
+    array[2]=new int[3];
+
+    array[0][0]=1;
+    array[0][1]=0;
+
+    array[1][0]=5;
+    array[1][1]=4;
+
+    std::cout<<array[1][1] <<std::endl;
+
+    delete[] array[0];
+    delete[] array[1];
+    delete[] array[2];
+    delete[] array;
+```    
+
+
+```cpp
+int my3DArray[2][3][2] ={
+			{{1, 2},{3, 4},{5, 6}},
+			{{7, 8}, {9, 10}, {11, 12}}
+	};
+```
+
+
+
+
+
+
+## Sending Multidimensional Array to Functions
+
+1. Fixed Size: it works for automatic (usually stack-living) arrays only i.e. the dimensions should be known at compile time. 
+
+The parameter is a 2D array, you have to specify the size of all dimension except the first one.
+
+this one expect exactly 3x4:
+```cpp
+void foo(int a[4][3])
+{
+}
+```
+
+we can omit (only) the first size dimension(the most left one)
+```cpp
+void foo(int a[][3], int size)
+{
+}
+```
+
+2. Variable Size
+
+
+a pointer to the decayed type:
+
+```cpp
+void foo(int *a[10], int size)
+{
+}
+```
+
+```cpp
+int *array[10];
+for(int i = 0; i < 10; i++)
+    array[i] = new int[10];
+void foo(int *a[10]) 
+{
+}
+
+foo(array);
+```
+
+
+pointer to a pointer:
+
+```cpp
+void foo(int **a, int m, int n)
+{
+}
+```
+
+
+```cpp
+ int **pp_array;
+    int m,  n;
+
+    m=4;
+    n=5;
+
+    pp_array = new int *[n];
+    for(int i = 0; i <m; i++)
+        pp_array[i] = new int[n];
+```
+or 
+
+```cpp
+
+int first2DArray[3][5] ={
+{ 1, 2, 3, 4, 5, }, // row 0
+{ 6, 7, 8, 9, 10, }, // row 1
+{ 11, 12, 13, 14, 15 } // row 2
+};
+
+foo((int**)&first2DArray[0][0], 3, 5);
+```
+
+
+Refs: [1](https://stackoverflow.com/questions/8767166/passing-a-2d-array-to-a-c-function)
+
+
+
 
 # std::array
+`std::array` is a container that encapsulates fixed size arrays.
+
+```cpp
+std::array<int, 3> a2 = {1, 2, 3};
+std::array<std::string, 2> a3 = { std::string("a"), "b" };
+```
+container operations are supported:
+
+```cpp
+std::sort(a2.begin(), a2.end());
+std::reverse_copy(a2.begin(), a2.end(), std::ostream_iterator<int>(std::cout, " "));
+```
+
+to_array:
+```cpp
+std::to_array("foo");        // creates std::array<char, 4>{ 'f', 'o', 'o', '\0' }
+std::array{"foo"};           // creates std::array<const char*, 1>{ +"foo" }
+```
+
+checking is array:
+
+```cpp
+char *s  = "Behnam";
+std::cout << std::boolalpha;
+std::cout << std::is_array<Foo>::value << '\n';
+std::cout << std::is_array<Foo[]>::value << '\n';
+std::cout << std::is_array<Foo[3]>::value << '\n';
+std::cout << std::is_array<float>::value << '\n';
+std::cout << std::is_array<int>::value << '\n';
+std::cout << std::is_array<int[]>::value << '\n';
+std::cout << std::is_array<int[3]>::value << '\n';
+std::cout << std::is_array<std::array<int, 3>>::value << '\n';
+```
 
 
 # Span
+
+
+[code](../src/arrays.cpp)
--- src/arrays.cpp
+++ src/arrays.cpp
@@ -10,103 +10,79 @@
 #include <iostream>
 #include <algorithm>
 
-/*differences between arrays and pointers
-what you declare in a parameter list is treated special. There are such situations where things don't make
- sense as a function parameter in C. These are
 
-1)Functions as parameters
-2)Arrays as parameters
-
-size of an array dimension is part of the type in C (and an array whose dimension size isn't given has an incomplete type).
-
-*/
-
-void sendingArray(int arrayToPrint[],int size)
+// this one expect exactly 3x4
+void foo(int a[4][3])
 {
-    std::cout<<"Print Array, in this function we send the size of the array as well"<<std::endl;
-	for(int i=0;i<size;i++)
-	{
-        std::cout<<arrayToPrint[i]<<std::endl;
-	}
-}
-
-void sendingArray(int arrayToPrint[5])
-{
-    std::cout<<"Print Array, in This function we have defined the size of the passed array in function definition"<<std::endl;
-	for(int i=0;i<5;i++)
-	{
-        std::cout<<arrayToPrint[i]<<std::endl;
-	}
-}
-
-void sendingMultidimensionalArray( int x, int y, int z ,int  arrayToPrint[][3][2])
-{
-	for(int i=0;i<x;i++)
-	{
-		for(int j=0;j<y;j++)
-		{
-			for(int k=0;k<z;k++)
-			{
-                std::cout<<"Row # "<<i <<std::endl;
-                std::cout<<"Column # "<<j <<std::endl;
-                std::cout<<"Vertex # "<<k <<std::endl;
-                std::cout<<arrayToPrint[i][j][k] <<std::endl;
-			}
-		}
-	}
 }
 
 
-int arraysmain()
+//we can omit (only) the first size dimension(the most left one)
+void foo(int a[][3], int size)
 {
+}
 
-	int FirstArray[] = { 1, 2, 3, 4, 5 };
-    std::cout << "sizeof(FirstArray)="<<sizeof(FirstArray)<<std::endl; // prints 20 (5 elements * 4 bytes each)
-	const int ArraySize = 5;
+void foo(int *a[10], int size)
+{
+}
+
+void foo(int **a, int m, int n)
+{
+}
+
+void sendingArraytoFunctions()
+{
+    int array[2][3];
+    foo(array,3);
 
 
-    std::cout<<"*FirstArray"<< *FirstArray<<std::endl;
-    std::cout<<"*(FirstArray+2)"<< *(FirstArray+2)<<std::endl;
+    int *p_array[10];
+    for(int i = 0; i < 10; i++)
+        p_array[i] = new int[4];
+    foo(p_array,4);
 
-	//Iterators are similar to pointers
-    std::cout<<"Iterators are similar to pointers"<<std::endl;
-	for (int* p = FirstArray; p != FirstArray+ArraySize; p++)
-	{
-		//These are equal:
-		//FirstArray=&FirstArray[0]
-		//FirstArray+ArraySize=&FirstArray[ArraySize]
+
+    int **pp_array;
+    int m,  n;
+
+    m=4;
+    n=5;
+
+    pp_array = new int *[n];
+    for(int i = 0; i <m; i++)
+        pp_array[i] = new int[n];
+
+    int first2DArray[3][5] ={
+    { 1, 2, 3, 4, 5, }, // row 0
+    { 6, 7, 8, 9, 10, }, // row 1
+    { 11, 12, 13, 14, 15 } // row 2
+    };
+
+    foo((int**)&first2DArray[0][0], 3, 5);
+
+}
+
+
+void accessingArrayElements()
+{
+    int array[] = { 1, 2, 3, 4, 5 };
+    std::cout << "sizeof(array)="<<sizeof(array)<<std::endl; // prints 20 (5 elements * 4 bytes each)
+    const int number_of_elemenets = sizeof(array)/sizeof(int);
+    std::cout << "number of elemenets = "<<number_of_elemenets<<std::endl;
+
+    //These are equal:
+    //array<=>&array[0]
+    //array+index<=>&array[index]
+    std::cout<<"*array="<< *array<<std::endl;
+    std::cout<<"*(array+2)="<< *(array+2)<<std::endl;
+
+    for (int* p = array; p != array+number_of_elemenets; p++)
+    {
         std::cout << *p << std::endl;
-	}
-
-
-	int First2DArray[3][5] ={
-	{ 1, 2, 3, 4, 5, }, // row 0
-	{ 6, 7, 8, 9, 10, }, // row 1
-	{ 11, 12, 13, 14, 15 } // row 2
-	};
-
-//	Two-dimensional arrays with initializer lists can omit (only) the first size dimension(the most left one):
-//C does not have true multidimensional arrays. you can have arrays of arrays, so like
-// FirstArray[] = { 1, 2, 3, 4, 5 }  which we didn't declare the dimension, we could have:
-	int Second2DArray[][5] ={{ 1, 2, 3, 4, 5, },{ 6, 7, 8, 9, 10, },{ 11, 12, 13, 14, 15 }};
-
-
-    sendingArray(FirstArray);
-    sendingArray(FirstArray,5);
-//	It can also be called:
-//	SendingArray(&FirstArray[0],5);
-
-	int Second3DArray[2][3][2] ={
-			{{1, 2},{3, 4},{5, 6}},
-			{{7, 8}, {9, 10}, {11, 12}}
-	};
-	//If you want to pass a multi-dimensional array to a function, the simplest way is to specify all of the array sizes
-	//except the leftmost; e.g.,
-
-    sendingMultidimensionalArray(2,3,2,Second3DArray);
-
-	return 0;
+    }
 }
+
+
 
 void cppArrayExample()
 {
@@ -120,38 +96,15 @@ void cppArrayExample()
 
     std::cout << '\n';
 
-
-    //make_array
-    //to_array
 }
 
-void twoDimentionalArray()
-{
-    int **array;
-    array=new int *[3];
-    array[0]=new int[2];
-    array[1]=new int[4];
-    array[2]=new int[3];
 
-    array[0][0]=1;
-    array[0][1]=0;
-
-    array[1][0]=5;
-    array[1][1]=4;
-
-    std::cout<<array[1][1] <<std::endl;
-
-    delete[] array[0];
-    delete[] array[1];
-    delete[] array[2];
-    delete[] array;
-}
 
 struct Foo {};
 
 void checkingIsArray()
 {
-    char *s  = "Behnam";
+    const char *s  = "Behnam";
     std::cout << std::boolalpha;
     std::cout << std::is_array<Foo>::value << '\n';
     std::cout << std::is_array<Foo[]>::value << '\n';
