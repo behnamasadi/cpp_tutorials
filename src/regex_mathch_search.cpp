53fe51a71f57742db118c28dc4aa1633daf20d0e
Behnam Asadi <behnam.asadi@gmail.com>
2023-04-02 20:19:36 +0200

    regex updated

--- src/regex_mathch_search.cpp
+++ src/regex_mathch_search.cpp
@@ -1,10 +1,28 @@
+#include <algorithm>
+#include <filesystem>
+#include <iostream>
+#include <optional>
 #include <regex>
 
-
-
-
-
-int main(int argc, char **argv)
-{
-
+std::optional<std::string>
+findFileWithRegexPattern(const std::string &searchPath,
+                         const std::regex &regex) {
+  const std::filesystem::directory_iterator end;
+  try {
+    for (std::filesystem::directory_iterator iter{searchPath}; iter != end;
+         iter++) {
+      const std::string file = iter->path().filename().string();
+      if (std::filesystem::is_regular_file(*iter)) {
+        // regex can be verified at https://regex101.com/
+        if (std::regex_match(file, regex)) {
+          std::cout << "hash exist: " << file.c_str();
+          return (iter->path().string());
+        }
+      }
+    }
+  } catch (std::exception &) {
+  }
+  return std::nullopt;
 }
+
+int main(int argc, char **argv) {}
