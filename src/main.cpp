6ea91181a6250522bc3ad546acd5d24c0c9f8ce1
Behnam Asadi <behnam.asadi@gmail.com>
2023-12-24 21:52:57 +0100

    signal updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,52 @@
-#include <algorithm>
 #include <cmath>
 #include <exception>
 #include <iomanip>
 #include <iostream>
-#include <list>
-#include <map>
 #include <memory>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
 
-int main() {}
+struct Foo {
+  std::string id;
+  Foo(std::string id) : id(id) {
+
+    std::cout << id << " constructor" << std::endl;
+  }
+  ~Foo() { std::cout << id << " destructor" << std::endl; }
+};
+
+Foo globalFoo("global object");
+static Foo staticFoo("static object");
+
+void runBeforeTermination() {
+  std::cout << "cleaning up temp dir before termination..." << std::endl;
+}
+void abortExample() {
+  Foo fooObject("abortExample");
+  std::shared_ptr<Foo> fooObject_ptr(new Foo("abortExample_ptr"));
+  atexit(runBeforeTermination);
+  abort();
+}
+
+int main() {
+  int array[5] = {0, 1, 2, 3, 4};
+
+  std::cout << "Original array values:\n";
+  for (int i = 0; i < 5; i++) {
+    std::cout << "array[" << i << "] = " << array[i] << "\n";
+  }
+
+  // Overflowing the array
+  // Writing beyond the bounds of the array
+  std::cout << "\nWriting beyond the bounds of the array...\n";
+  for (int i = 0; i < 10;
+       i++) { // This loop intentionally goes well beyond the array's bounds
+    array[i] = i;
+  }
+
+  // std::cout << "\nReading values after overflow:\n";
+  // // Reading values after overflow
+  // for (int i = 0; i < int(100);
+  //      i++) { // Again, intentionally reading beyond the array's bounds
+  //   std::cout << "array[" << i << "] = " << array[i] << "\n";
+  //   std::cout << "i = " << i << "\n";
+  // }
+}
