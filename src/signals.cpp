209588cd0396880e4a5b41f2e8c2d6b0ed69a200
Behnam Asadi <behnam.asadi@gmail.com>
2023-12-24 21:52:57 +0100

    signal updated

--- src/signals.cpp
+++ src/signals.cpp
@@ -3,32 +3,32 @@
 #include <iostream>
 #include <unistd.h> // getpid()
 
-sig_atomic_t s_value = 0;
-void handle(int signum) { s_value = signum; }
-
-void SIGSEGVhandle(int signum) {
+void SIGSEGV_Handler(int signum) {
   std::cout << "oh my god! segmenation fault happened" << std::endl;
   printf("Process %d got signal %d\n", getpid(), signum);
-
   // kill(getpid(), signum);
   exit(signum);
 }
 
-void SIGExample() {
-  // SIGILL,SIGINT,SIGSEGV,SIGTERM,SIGABRT,SIGFPE
-  signal(SIGTERM, handle);
+void SIGINT_Handler(int signum) {
+  std::cout << "Interrupt signal (" << signum << ") received.\n";
+  // Cleanup and close up logic
+  exit(signum);
+}
+
+sig_atomic_t s_value = 0;
+void SIGTERM_Handler(int signum) { s_value = signum; }
+
+int main() {
+  // signal(SIGSEGV, SIGSEGV_Handler);
+  // int *p;
+  // *p = 10;
+
+  // //  Register signal SIGINT and signal handler
+  // signal(SIGINT, SIGINT_Handler);
+
+  signal(SIGTERM, SIGTERM_Handler);
   std::cout << "Before called Signal = " << s_value << std::endl;
   raise(SIGTERM);
   std::cout << "After called Signal = " << s_value << std::endl;
-}
-
-int main() {
-  // first example
-  // SIGExample();
-
-  // second example
-  signal(SIGSEGV, SIGSEGVhandle);
-  // signal(SIGSEGV, SIG_DFL);  // restore default behavior
-  int *p;
-  *p = 10;
 }
